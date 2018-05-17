b9d4a11dc9b3e020056477221e127718efa95582
Alex Ponomarev <alex.ponomarev7@ya.ru>
2018-05-18 01:55:00 +0300

    Cmake

---
+++ cmake/A/script.py
@@ -0,0 +1,3 @@
+f_w = open("index.h", "w")
+
+print('#include <map>', file=f_w)
---
+++ cmake/B/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "A/index.h"
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
